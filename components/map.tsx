ebbdc327c9c0111141de8e481f6f776f46bbe0d4
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-07-21 15:15:18 -0400

    update

--- components/map.tsx
+++ components/map.tsx
@@ -1,1 +1,45 @@
-export default Map;
+import { useState, useMemo, useCallback, useRef } from "react";
+import {
+    GoogleMap,
+    Marker,
+    DirectionsRenderer,
+    Circle,
+    MarkerClusterer,
+} from "@react-google-maps/api";
+import Places from "./places";
+import Distance from "./distance";
+
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type DirectionsResult = google.maps.DirectionsResult;
+type MapOptions = google.maps.MapOptions; 
+
+
+export default function Map() {
+    return <div>Map</div>;
+}
+
+const defaultOptions = {
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false, 
+    visible: true,
+};
+
+const closeOptions = {
+    ...defaultOptions,
+    zIndex: 3,
+    fillOpacity: 0.05,
+    strokeColor: "#8BC34A" ,
+    fillColor: "#8BC34A" ,
+};
+
+const middleOptions = {
+    ...defaultOptions,
+    zIndex: 2,
+    fillOpacity: 0.05,
+    strokeColor: "#FBC02D" ,
+    fillColor: "#FBC02D",
+};
